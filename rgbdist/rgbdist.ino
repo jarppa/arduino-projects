937f29fe89c252afe358b24aaf3eaf73f94b0426
Jari Tervonen <jjtervonen@gmail.com>
2014-03-31 23:17:00 +0300

    Add more LEDs to rgbdist.

--- rgbdist/rgbdist.ino
+++ rgbdist/rgbdist.ino
@@ -1,143 +1,172 @@
 /*
-    Basic Pin setup:
-    ------------                                  ---u----
-    ARDUINO   13|-> SCLK (pin 25)           OUT1 |1     28| OUT channel 0
-              12|                           OUT2 |2     27|-> GND (VPRG)
-              11|-> SIN (pin 26)            OUT3 |3     26|-> SIN (pin 11)
-              10|-> BLANK (pin 23)          OUT4 |4     25|-> SCLK (pin 13)
-               9|-> XLAT (pin 24)             .  |5     24|-> XLAT (pin 9)
-               8|                             .  |6     23|-> BLANK (pin 10)
-               7|                             .  |7     22|-> GND
-               6|                             .  |8     21|-> VCC (+5V)
-               5|                             .  |9     20|-> 2K Resistor -> GND
-               4|                             .  |10    19|-> +5V (DCPRG)
-               3|-> GSCLK (pin 18)            .  |11    18|-> GSCLK (pin 3)
-               2|                             .  |12    17|-> SOUT
-               1|                             .  |13    16|-> XERR
-               0|                           OUT14|14    15| OUT channel 15
-    ------------                                  --------
-
-    -  Put the longer leg (anode) of the LEDs in the +5V and the shorter leg
-         (cathode) in OUT(0-15).
-    -  +5V from Arduino -> TLC pin 21 and 19     (VCC and DCPRG)
-    -  GND from Arduino -> TLC pin 22 and 27     (GND and VPRG)
-    -  digital 3        -> TLC pin 18            (GSCLK)
-    -  digital 9        -> TLC pin 24            (XLAT)
-    -  digital 10       -> TLC pin 23            (BLANK)
-    -  digital 11       -> TLC pin 26            (SIN)
-    -  digital 13       -> TLC pin 25            (SCLK)
-    -  The 2K resistor between TLC pin 20 and GND will let ~20mA through each
-       LED.  To be precise, it's I = 39.06 / R (in ohms).  This doesn't depend
-       on the LED driving voltage.
-    - (Optional): put a pull-up resistor (~10k) between +5V and BLANK so that
-                  all the LEDs will turn off when the Arduino is reset.
-
-    If you are daisy-chaining more than one TLC, connect the SOUT of the first
-    TLC to the SIN of the next.  All the other pins should just be connected
-    together:
-        BLANK on Arduino -> BLANK of TLC1 -> BLANK of TLC2 -> ...
-        XLAT on Arduino  -> XLAT of TLC1  -> XLAT of TLC2  -> ...
-    The one exception is that each TLC needs it's own resistor between pin 20
-    and GND.
-
-    This library uses the PWM output ability of digital pins 3, 9, 10, and 11.
-    Do not use analogWrite(...) on these pins.
-
-    This sketch does the Knight Rider strobe across a line of LEDs.
-
-    Alex Leone <acleone ~AT~ gmail.com>, 2009-02-03 */
-
+    Author: Jari Tervonen <jjtervonen ~Ã„T~ gmail.com>, 2013-
+    
+    Based on TLC and Mirf library examples by:
+    Alex Leone <acleone ~AT~ gmail.com>, 2009-02-03
+   and
+    Unknown author. 
+*/
 #include "Tlc5940.h"
-
-const int trigPin = 8;
-const int echoPin = 7;
-
-const int RED = 1;
-const int GREEN = 2;
-const int BLUE = 3;
+//#include <SPI.h>
+//#include <Mirf.h>
+//#include <nRF24L01.h>
+//#include <MirfHardwareSpiDriver.h>
 
 void setup()
 {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  digitalWrite(trigPin, LOW);
   Serial.begin(9600);
+  Serial.setTimeout(10);
   /* Call Tlc.init() to setup the tlc.
      You can optionally pass an initial PWM value (0 - 4095) for all channels.*/
-  Tlc.init(0);
+  Tlc.init(1000);
+  
+  /*Mirf.cePin = 4;
+  Mirf.csnPin = 2;
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR((byte *)"hmd01");
+  Mirf.payload = sizeof(unsigned long);*/
+  
+  delay(5000);
 }
 
-/* This loop will create a Knight Rider-like effect if you have LEDs plugged
-   into all the TLC outputs.  NUM_TLCS is defined in "tlc_config.h" in the
-   library folder.  After editing tlc_config.h for your setup, delete the
-   Tlc5940.o file to save the changes. */
+static byte input [4];
+static uint32_t control = 0x00000000;
+//static uint8_t buf[VW_MAX_MESSAGE_LEN];
+//static uint8_t buflen = VW_MAX_MESSAGE_LEN;
+boolean first_run = true;
+boolean hmd_connected = false;
+
+unsigned long ACK = 0xaaaaaaaa;
+unsigned long time, last_update_time = 0;
 
 void loop()
 {
-  long dist = measureDistance();
-  //Serial.println(dist);
-  updateColors(dist);
+ /* Mirf.setTADDR((byte *)"serv1");
   
-  delay(100);
-}
-static int LED_COUNT = 5;
+  time = millis();
+  if (!hmd_connected || (time - last_update_time > 2000)) {
+    Mirf.send((byte *)&ACK);
+    last_update_time = millis();
+    
+    while(Mirf.isSending()){}
+    Serial.println("Finished sending");
+    delay(10);
+    while(!Mirf.dataReady()) {
+      Serial.println("Waiting");
+      if ( ( millis() - time ) > 1000 ) {
+        Serial.println("Timeout on response from server!");
+        hmd_connected = false;
+        return;
+      }
+    }
+    hmd_connected = true;
+  }
+  Mirf.getData((byte *) &input);
+  
+  control = (uint32_t) ( (uint32_t)(input[0]) | ((uint32_t(input[1])<<8)) | ((uint32_t(input[2])<<16)) | ((uint32_t(input[3])<<24))  );
+  input[0] = 0;
+  input[1] = 0;
+  input[2] = 0;
+  input[3] = 0;
+  //Serial.print("Got: ");Serial.println(control, BIN);
+  updateColors(control);*/
+    
+  if (Serial.available()) {
+    Serial.readBytes((char*)&input, 4);
+    first_run = false;
+    control = (uint32_t) ( (uint32_t)(input[0]) 
+              | ((uint32_t(input[1])<<8)) 
+              | ((uint32_t(input[2])<<16)) 
+              | ((uint32_t(input[3])<<24)) );
+    input[0] = 0;
+    input[1] = 0;
+    input[2] = 0;
+    input[3] = 0;
+    Serial.print("Got: ");Serial.println(control, BIN);
+    updateColors(control);
+  }
+  else if (first_run) {
+    demo();
+  }
+  /*if (vw_get_message(buf, &buflen)){
+    int i;
 
-void updateColors(long d) { 
-  int i = 0;
+    for (i = 0; i < buflen; i++)
+    {
+      input[i%4] = buf[i];
+      //Serial.print(buf[i], HEX);
+      //Serial.print(" ");
+    }
+  }*/
+  //long dist = measureDistance();
+  //Serial.println(dist);
+  delay(10);
+}
+static int LEDS_IN_GROUP = 16;
+static int LED_GROUPS = 2;
+static int LED_COUNT = LED_GROUPS*LEDS_IN_GROUP;
+static int BRIGHTNESS_BITS [] = {14,15,30,31};
+static int BRIGHTNESS_BIT_COUNT = 4;
+
+
+boolean not_in(int x, int *y, int len) {
+  for(int i=0; i<len; i++) {
+    if (y[i] == x)
+      return false;
+  }
+  return true;
+}
+
+void updateColors(uint32_t c) { 
+  int i,j = 0;
+  uint8_t brightness = 0;
+  
   Tlc.clear();
   //Serial.println(map(d,400,4,0,4095));
-  for (i=0;i<LED_COUNT*3;i+=3) {
-    
-    if (d > 4) {
-      Tlc.set(i, map(d,400,4,0,4095));
-      Tlc.set(i+1, map(d,4,400,0,4095));
-      Tlc.set(i+2, map(d,4,400,0,4095));
+ 
+  brightness = (uint8_t)((c&0xf0000000)>>28);
+  
+  for (i=0,j=0;i<LED_COUNT;i++) {
+    if (i == 14 || i == 15 || i == 30 || i == 31) {
+       continue;
     }
-    else {
-      Tlc.set(i, 4095);
-      Tlc.set(i+1, 0);
-      Tlc.set(i+2, 0);
+    if ( (uint32_t) ((c >> j) & 0x01) == 1 ){
+      //Tlc.set(i, map( ((c&0xff000000)>>24), 0, 255, 0, 4095));
+      Tlc.set(i, map( brightness, 0, 16, 0, 4095));
     }
-    
+    j++;
   }
   Tlc.update();
 }
 
-static long RANGE_ECHO_MAX = 23200;
+uint32_t demo_val = 0x85555555;
 
-long measureDistance() {
-  float d = 0;
-  long trig_t,echo_t,diff = 0;
-  float result  = 0;
-  int i,meas = 0;
-  //Serial.println("Measure");
-  
-  for (i=0;i<5;i++) {
-    //Serial.print("for:");Serial.println(i);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-  
-    while ( digitalRead(echoPin) == LOW) ;
-    trig_t = micros();
-  
-    while (digitalRead(echoPin) == HIGH) ;
-    echo_t = micros();
-    diff = echo_t-trig_t;
+void demo() {
+  int i = 0;
+  Tlc.clear();
+  //Serial.println(map(d,400,4,0,4095));
+  for (i=0;i<14;i++) {
     
-    if (diff < 0 || diff > RANGE_ECHO_MAX){
-      Serial.print("Value error:");Serial.println(diff);
-      delay(100);
-      continue;
-    }
-    meas++;
-    
-    d += diff/58.0;
-    Serial.println(d/(meas));
-    delay(100);
+    if ( (uint32_t) ((demo_val >> i) & 0x01) == 1 )
+      Tlc.set(i, 2000);
   }
-  if (meas > 0)
-    return (long)(d/meas);
-  else
-    return long(400);
+  for (i=14;i<16;i++) {
+    Tlc.set(i, 0);
+  }
+  
+  for (i=16;i<30;i++) {
+    
+    if ( (uint32_t) ((demo_val >> (i-4)) & 0x01) == 1 )
+      Tlc.set(i, 2000);
+  }
+  for (i=30;i<32;i++) {
+    Tlc.set(i, 0);
+  }
+  
+  Tlc.update();
+  demo_val ^= 0x0aaaaaaa;
+  demo_val ^= 0x05555555;
+  delay(500);
 }
+
